d76a948e2f3933a3d2853af9cb0b17f919683df8
samjoash9 <113993739+samjoash9@users.noreply.github.com>
2024-12-18 18:35:20 +0800

    parallax done

---
+++ scenes/parallax_intro/parallax_background_2.gd
@@ -0,0 +1,12 @@
+extends ParallaxBackground
+
+@onready var parallax_layer: ParallaxLayer = $ParallaxLayer
+@onready var parallax_layer_2: ParallaxLayer = $ParallaxLayer2
+@onready var parallax_layer_3: ParallaxLayer = $ParallaxLayer3
+@onready var parallax_layer_4: ParallaxLayer = $ParallaxLayer4
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	parallax_layer_2.motion_offset.x -= 8.2 * delta
+	parallax_layer_3.motion_offset.x -= 14 * delta
+	parallax_layer_4.motion_offset.x -= 18 * delta
