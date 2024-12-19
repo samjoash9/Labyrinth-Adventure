9e2b7773b1c82082745f8aedfb662fada2edca76
jlibdev <jlibandodev@gmail.com>
2024-12-19 15:27:32 +0800

    asd

--- scenes/Common/player_camera.gd
+++ scenes/Common/player_camera.gd
@@ -2,5 +2,5 @@ extends Camera2D
 
 class_name Camera
 
-@onready var player: Player = get_parent()
+@onready var player: playerCharacter = get_parent()
 @onready var camera_limit_manager = $CameraLimitManager
--- scenes/central_hub.gd
+++ scenes/central_hub.gd
@@ -4,8 +4,9 @@ class_name central_hub
 
 @onready var initial_camera_limiter = $CameraLimiters/camera_limiter_leftBottom
 @onready var camera2 = $CameraLimiters/camera_limiter_rightBottom
-@onready var player: Player = $Player
+@onready var player: playerCharacter = $Player
 
 func _ready() -> void:
-	player.camera.camera_limit_manager.set_limiter(initial_camera_limiter, false)
-	player.camera.camera_limit_manager.set_limiter(camera2, false)
+	#player.camera.camera_limit_manager.set_limiter(initial_camera_limiter, false)
+	#player.camera.camera_limit_manager.set_limiter(camera2, false)
+	pass
