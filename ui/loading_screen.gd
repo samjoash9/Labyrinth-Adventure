07af3b58807db03711ffe378544a6560fef882bd
samjoash9 <113993739+samjoash9@users.noreply.github.com>
2024-12-24 12:09:04 +0800

    central hub fixed

--- ui/loading_screen.gd
+++ ui/loading_screen.gd
@@ -9,6 +9,9 @@ func _on_maze_generator_map_loaded() -> void:
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
